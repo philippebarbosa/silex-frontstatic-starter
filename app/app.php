732d8c7458a66a53f8f0b43d537b970f9b23ee71
Philippe Barbosa <contact@philippebarbosa.com>
2016-04-14 16:55:25 +0200

    Footer for base.html, added assets route

--- app/app.php
+++ app/app.php
@@ -8,6 +8,17 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 ));
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 
+// Set path for assets
+$app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
+    $twig->addFunction(new \Twig_SimpleFunction('asset', function ($asset) {
+        // implement whatever logic you need to determine the asset path
+
+        return sprintf('http://localhost/%s', ltrim($asset, '/'));
+    }));
+
+    return $twig;
+}));
+
 // Routes
 $app->get('/', function () use ($app) {
     return $app['twig']->render('index.html.twig', array(
